4bfd2da8d5a87fdddeb973e95730c39dc57d14ee
esra-polat <esrapolat1293@gmail.com>
2020-09-29 13:39:59 +0300

    exercise was completed
    .vscode file deleted

---
+++ Exercise 5 (Regularized Linear Regression and Bias v.s. Variance)/ex5/learningCurve.m
@@ -0,0 +1,16 @@
+function [error_train, error_val] = learningCurve(X, y, Xval, yval, lambda)
+
+m = size(X, 1);
+
+error_train = zeros(m, 1);
+error_val   = zeros(m, 1);
+
+for i = 1:m
+      X_train = X(1:i,:);
+      y_train = y(1:i);
+      theta = trainLinearReg(X_train, y_train, lambda);
+      error_train(i) = linearRegCostFunction(X_train, y_train, theta, 0); % for lambda = 0;
+      error_val(i)   = linearRegCostFunction(Xval, yval, theta, 0); % for lambda = 0;
+end
+
+end
---
+++ Exercise 5 (Regularized Linear Regression and Bias v.s. Variance)/ex5/linearRegCostFunction.m
@@ -0,0 +1,19 @@
+function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
+
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+h_theta = X * theta;
+
+J = (1/(2*m)) * sum((h_theta - y).^2) + (lambda/(2*m)) * sum(theta(2:end).^2);
+
+
+grad = 1 / m * X' * (h_theta - y);
+grad(2:end) += (lambda / m) * theta(2:end);
+
+grad = grad(:);
+
+end
